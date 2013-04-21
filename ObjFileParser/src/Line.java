994160b19611855d2c7b345e75aaf472e534d4fb
imaspiderman <imaspiderman@hotmail.com>
2013-04-21 16:59:25 -0400

    Changed the storage format to distinct vertices and line endpoint indexes

--- ObjFileParser/src/Line.java
+++ ObjFileParser/src/Line.java
@@ -4,59 +4,41 @@
  *
  */
 public class Line {
-	int[] startVertex;
-	int[] endVertex;
+	int startVertex = 0;
+	int endVertex = 0;
 	
 	/**
 	 * Constructor
-	 * @param s start vertex as array {x,y,z}
-	 * @param e end vertex as array {x,y,z}
+	 * @param s start vertex
+	 * @param e end vertex
 	 */
-	public Line(int[] s, int[] e){
-		startVertex = new int[3];
-		endVertex = new int[3];
-		
-		startVertex[0] = s[0];
-		startVertex[1] = s[1];
-		startVertex[2] = s[2];
-		
-		endVertex[0] = e[0];
-		endVertex[1] = e[1];
-		endVertex[2] = e[2];
+	public Line(int s, int e){
+		startVertex = s;
+		endVertex = e;
 	}
 	
 	public Line(){
-		startVertex = new int[3];
-		endVertex = new int[3];
 	}
 	
 	/**
 	 * Returns the starting vertex
 	 * @return
 	 */
-	public int[] getStart(){
+	public int getStart(){
 		return startVertex;
 	}
 	
 	public void printLine(){
-		System.out.print(String.format("sx: %d sy: %d sz: %d ex: %d ey: %d ez: %d ",
-				startVertex[0],
-				startVertex[1],
-				startVertex[2],
-				endVertex[0],
-				endVertex[1],
-				endVertex[2]
+		System.out.print(String.format("start: %d end: %d ",
+				startVertex,
+				endVertex
 				));
 	}
 	
 	public String printLineCoordinates(){
-		return String.format("%d,%d,%d,%d,%d,%d",
-				startVertex[0],
-				startVertex[1],
-				startVertex[2],
-				endVertex[0],
-				endVertex[1],
-				endVertex[2]
+		return String.format("%d,%d",
+				startVertex,
+				endVertex
 				);
 	}
 	
@@ -64,7 +46,7 @@ public class Line {
 	 * Returns the ending vertex
 	 * @return
 	 */
-	public int[] getEnd(){
+	public int getEnd(){
 		return endVertex;
 	}
 	
@@ -76,21 +58,13 @@ public class Line {
 	public boolean compare(Line l){
 		boolean b = false;
 		
-		if(startVertex[0] == l.getStart()[0] &&
-		   startVertex[1] == l.getStart()[1] &&
-		   startVertex[2] == l.getStart()[2] &&
-		   endVertex[0] == l.getEnd()[0] &&
-		   endVertex[1] == l.getEnd()[1] &&
-		   endVertex[2] == l.getEnd()[2]
-				) b=true;
+		if(startVertex == l.getStart() &&
+		   endVertex == l.getEnd()
+        ) b=true;
 		
-		if(startVertex[0] == l.getEnd()[0] &&
-				   startVertex[1] == l.getEnd()[1] &&
-				   startVertex[2] == l.getEnd()[2] &&
-				   endVertex[0] == l.getStart()[0] &&
-				   endVertex[1] == l.getStart()[1] &&
-				   endVertex[2] == l.getStart()[2]
-						) b=true;
+		if(startVertex == l.getEnd() &&
+		   endVertex == l.getStart()
+		) b=true;
 		
 		return b;
 	}
--- ObjFileParser/src/ObjParser.java
+++ ObjFileParser/src/ObjParser.java
@@ -23,20 +23,42 @@ public class ObjParser {
 	
 	private void writeOutData(){
 		java.util.Iterator<Line>i = lines.iterator();
+		java.util.Iterator<Integer>v = vertices.iterator();
 		Line t = new Line();
-		Integer c=0;
 		
 		System.out.print("const s32 obj[] = {");
-		System.out.print((lines.size()*6) + ", //total number of vertex points\n");
-		System.out.print("\t2,\n");
-		//faces
+		System.out.print(vertices.size() + ", //total number of vertices\n");
+		System.out.print("\t" + (lines.size()*2) + ", //total number of line end points\n");
+		System.out.print("\t2,\n\t//Distinct vertices\n");
+		//vertices
+		boolean bFirst = true;
+		int count = 0;
+		while(v.hasNext()){
+			Integer vert = v.next();
+			
+			System.out.print((bFirst)?("\t" + vert.toString()):("," + vert.toString()));
+			bFirst = false;
+			
+			count++;
+			if(count==12){
+				System.out.print("\n\t");
+				count = 0;
+			}
+		}
+		//lines
+		count = 0;
+		bFirst = true;
 		while(i.hasNext()){
-			c+=1;
 			t = i.next();
 			
-			System.out.print("\t" + t.printLineCoordinates());
+			System.out.print((bFirst)?("\n\t//Vertex indexes\n\t," + t.printLineCoordinates()):("," + t.printLineCoordinates()));
+			bFirst = false;
 			
-			if(i.hasNext()) System.out.print(",\n");
+			count++;
+			if(count==8){
+				System.out.print("\n\t");
+				count = 0;
+			}
 		}
 		System.out.print("};\n");
 	}
@@ -74,22 +96,13 @@ public class ObjParser {
 				}
 				
 				if(type.equals("f")){
-					int v1,v2,v3,v4,v5,v6,vt;
+					int v1,v2;
 					
-					String v = (pieces[1].indexOf("/")>-1)?(pieces[1].split("/")[0]):(pieces[1]);
-					vt = (Integer.parseInt(v)-1)*3;
-					v1 = vertices.get(vt);
-					v2 = vertices.get(vt+1);
-					v3 = vertices.get(vt+2);
-					
+					v1 = Integer.parseInt((pieces[1].indexOf("/")>-1)?(pieces[1].split("/")[0]):(pieces[1]));
 					for(int i=2; i<pieces.length; i++){
-						v = (pieces[i].indexOf("/")>-1)?(pieces[i].split("/")[0]):(pieces[i]);
-						vt = (Integer.parseInt(v)-1)*3;
-						v4 = vertices.get(vt);
-						v5 = vertices.get(vt+1);
-						v6 = vertices.get(vt+2);
+						v2 = Integer.parseInt((pieces[i].indexOf("/")>-1)?(pieces[i].split("/")[0]):(pieces[i]));
 						
-						Line l = new Line(new int[]{v1,v2,v3}, new int[]{v4,v5,v6});
+						Line l = new Line(v1, v2);
 						
 						boolean add = true;
 						for(int j=0; j<lines.size(); j++){
@@ -101,20 +114,14 @@ public class ObjParser {
 						
 						if(add) lines.add(l);
 						
-						v1 = v4;
-						v2 = v5;
-						v3 = v6;
+						v1=v2;
 						
 						//The last vertex of a face connects to the first vertex
 						//with a line so this creates that final line
 						if(i==pieces.length-1){
-							v = (pieces[1].indexOf("/")>-1)?(pieces[1].split("/")[0]):(pieces[1]);
-							vt = (Integer.parseInt(v)-1)*3;
-							v4 = vertices.get(vt);
-							v5 = vertices.get(vt+1);
-							v6 = vertices.get(vt+2);
+							v2 = Integer.parseInt((pieces[i].indexOf("/")>-1)?(pieces[i].split("/")[0]):(pieces[i]));
 							
-							l = new Line(new int[]{v1,v2,v3}, new int[]{v4,v5,v6});
+							l = new Line(v1, v2);
 							
 							add = true;
 							for(int j=0; j<lines.size(); j++){
